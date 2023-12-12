95b81cdaf4480f2575384f64131cd485e8a05986
Esha <takia@appscode.com>
2023-12-12 17:50:02 +0600

    Go Code file on Structs

--- Structs.go
+++ Structs.go
@@ -1,1 +1,20 @@
 package main
+
+type person struct {
+	name string
+	age  int
+}
+
+func newperson(name string) *person {
+	p := person{name: name}
+	p.age = 55
+	return &p
+}
+
+type rectangle struct {
+	height, width float64
+
+	calc struct {
+		area, perimeter float64
+	}
+}
